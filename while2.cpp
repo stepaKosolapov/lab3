8292e69f7ce7572390921fc9105ba00f67a46157
stepaKosolapov <74272659+stepaKosolapov@users.noreply.github.com>
2020-11-27 11:26:48 +0300

    Create while2.cpp

---
+++ while2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A, B, counter;
+    cout << "Input A and B: ";
+    cin >> A >> B;
+    counter = 0;
+    while(A >= B)
+    {
+        counter += 1;
+        A -= B;
+    }
+    cout << "Answer: " << counter;
+    return 0;
+}
